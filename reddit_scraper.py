9c4d52ae2f232c2f16d6633f2a3434458f4e7870
mwilchek <mwilchek@gmu.edu>
2018-04-08 22:49:31 -0400

    edited scrapers and adding initial data

---
+++ reddit_scraper.py
@@ -0,0 +1,40 @@
+import praw
+from datetime import datetime
+from collections import namedtuple
+import pandas as pd
+
+# set PRAW API reqs
+reddit = praw.Reddit(client_id='nyk9gIgRru0IMQ',
+                     client_secret='liIo_b3xC2-oWQPpGWOZdU4H7Gs',
+                     user_agent='windows:com.praw.testapp:v1 (by /u/SkyNet_Developer)')
+
+# Create submission tuple object with features
+Submission = namedtuple('Submission', ['Date', 'Poster_Name', 'Post_Title', 'Link', 'Content'])
+
+# Create list to append data to
+data = []
+
+# Declare subreddit to scrape
+subreddit = reddit.subreddit('BaltimoreAndDCr4r')
+
+# Loop through subreddit's submissions collecting features and appending to list
+for submission in subreddit.new(limit=5000):
+    if "F4M" in submission.title:
+        time = submission.created
+        date = datetime.fromtimestamp(time)
+
+        author = submission.author
+        title = submission.title
+        link = submission.url
+        text = submission.selftext
+
+        data.append(Submission(date, author, title, link, text))
+
+# Create dataframe from list of data
+df = pd.DataFrame(data)
+
+# Declare filename for output
+filename = 'reddit_data.csv'
+
+# Output dataframe to csv file
+df.to_csv(filename, index=False, encoding='utf-8')
